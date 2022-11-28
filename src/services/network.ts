5e8d799e23968d0da1fb282a8db2e7ede5710e2f
Ktan <fredericmamath@gmail.com>
2022-11-28 17:18:14 +0100

    :rotating_light: Formats

--- src/services/network.ts
+++ src/services/network.ts
@@ -1,23 +1,21 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig } from 'axios'
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || ''
 
 export const axiosClient = axios.create({
   withCredentials: true,
   baseURL: BACKEND_URL,
-});
+})
 
 export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
-  const source = axios.CancelToken.source();
-  const promise = axiosClient({ ...config, cancelToken: source.token }).then(
-    ({ data }) => data
-  );
+  const source = axios.CancelToken.source()
+  const promise = axiosClient({ ...config, cancelToken: source.token }).then(({ data }) => data)
 
   // eslint-disable-next-line
   // @ts-ignore:external-dependencies
   promise.cancel = () => {
-    source.cancel("Query was cancelled by React Query");
-  };
+    source.cancel('Query was cancelled by React Query')
+  }
 
-  return promise;
-};
+  return promise
+}
