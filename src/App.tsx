fdadd199627901a31c56b2cdef298a9646ebc40a
Ktan <fredericmamath@gmail.com>
2022-11-28 20:25:33 +0100

    :construction: Adds react-router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,32 @@
 import { AppBar, Stack, Toolbar, Typography } from '@mui/material'
 import { SnackbarProvider } from "notistack";
 import { QueryClientProvider } from "react-query";
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 
 import DailyPage from 'pages/DailyPage/DailyPage'
-import NotFoundErrorPage from 'pages/NotFoundErrorPage'
 import SignInPage from 'pages/SignInPage'
 import { queryClient } from "services/reactQuery";
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <SignInPage />,
-    errorElement: <NotFoundErrorPage />,
-  },
-  {
-    path: "/daily",
-    element: <DailyPage />
-  }
-])
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <SnackbarProvider maxSnack={3}>
+    <BrowserRouter>
     <Stack gap={4}>
-      <AppBar position="sticky">
-        <Toolbar sx={{ gap: 2 }}>
-          <Typography variant="h6">Tool belt</Typography>
-          <Typography>Daily</Typography>
-          <Typography>Ceremony</Typography>
-        </Toolbar>
-      </AppBar>
-      <RouterProvider router={router} />
-    </Stack>
+        <AppBar position="sticky">
+          <Toolbar sx={{ gap: 2 }}>
+            <Link to="/"><Typography variant="h6">Tool belt</Typography></Link>
+            <Link to="/daily"><Typography>Daily</Typography></Link>
+            <Link to="/ceremony"><Typography>Ceremony</Typography></Link>
+          </Toolbar>
+        </AppBar>
+      </Stack>
+    <Routes>
+      <Route path="/" element={<SignInPage />} />
+      <Route path="/daily" element={<DailyPage />} />
+      <Route path="/ceremony" element={<SignInPage />} />
+    </Routes>
+    </BrowserRouter>
     </SnackbarProvider>
     </QueryClientProvider>
   )
