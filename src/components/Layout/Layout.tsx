ab68fb3ba0830b668b05715d76b6d8effc46ac69
Ktan <fredericmamath@gmail.com>
2022-11-29 23:02:46 +0100

    :construction: Adds some style to the login page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -22,20 +22,23 @@ const Layout = (props: Props) => {
   }
 
   return (
-    <Stack gap={4}>
+    <Stack gap={4}  sx={{ minHeight: authenticationCtx.isAuthenticated ? "calc(100vh - 64px - 32px - 32px - 32px)" : "100vh"}}>
+            {authenticationCtx.isAuthenticated && (
+
         <AppBar position="sticky">
           <Toolbar sx={{ gap: 2 }}>
             <Link to="/"><Typography variant="h6">Tool belt</Typography></Link>
-            {authenticationCtx.isAuthenticated && (
               <>
               <Link to="/daily"><Typography>Daily</Typography></Link>
               <Link to="/ceremony"><Typography>Ceremony</Typography></Link>
               <Button onClick={onLogout} variant="contained">Log out</Button>
               </>
-            )}
           </Toolbar>
         </AppBar>
+            )}
+        <Stack flex={1}>
         {children}
+        </Stack>
       </Stack>
   )
 }
--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Stack, TextField, Typography } from '@mui/material'
+import { Button, Stack, TextField, Typography } from '@mui/material'
 import { useAuthenticationCtx } from 'contexts/authenticationCtx';
 import { useSnackbar } from 'notistack';
 import { useEffect } from 'react';
@@ -55,15 +55,27 @@ const SignInPage = () => {
   };
 
   return (
-    <Stack>
-      <Typography variant="h2">Sign in</Typography>
-      <Card>
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField label="username" {...register("email")} />
-        <TextField label="password" {...register("password")} type="password" />
-        <Button type="submit">Sign in</Button>
+    <Stack direction="row" flex={1}>
+      <Stack flex={1}>
+        <img src="https://res.cloudinary.com/pokpok/image/upload/c_scale,q_15,w_567/v1669758803/conference-room-sketch_mwacth.jpg" style={{
+          height: "100%",
+          width: "100%"
+        }} />
+      </Stack>
+      <Stack flex={1} p={4}>
+      <Typography sx={{ textAlign: "center" }} variant="h2">Welcome</Typography>
+        <form onSubmit={handleSubmit(onSubmit)} style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1
+        }}>
+          <Stack gap={2} flex={1} justifyContent="center">
+            <TextField label="Username" {...register("email")} />
+            <TextField label="Password" {...register("password")} type="password" sx={{ mb: 4 }}/>
+            <Button type="submit" variant="contained">Sign in</Button>
+          </Stack>
         </form>
-      </Card>
+      </Stack>
     </Stack>
   )
 }
