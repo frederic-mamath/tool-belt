e3a9b380f2341278566ed7d5f34ff10f702fa8b2
Ktan <fredericmamath@gmail.com>
2022-12-02 21:15:40 +0100

    :art: Styles the truck a bit

--- src/components/Truck/Truck.tsx
+++ src/components/Truck/Truck.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mui/material'
+import { Divider, Stack, Typography } from '@mui/material'
 import { groupBy } from 'ramda'
 
 import { useGetClearstreamTickets } from 'generated/hook'
@@ -15,11 +15,12 @@ const Truck = () => {
   const clearstreamTicketByOwnerFirstName = mapByOwnerFirstName(clearstreamTicketOutboundDto)
 
 return (
-    <Stack maxWidth={512} direction="row" gap={2}>
+    <Stack direction="row" gap={2}>
      {Object.keys(clearstreamTicketByOwnerFirstName).map((user: string) => {
       return (
-        <Stack direction="column" key={`truck-column-${user}`}>
-          <div>{user}</div>
+        <Stack direction="column" key={`truck-column-${user}`} gap={2} flex={1}>
+          <Typography variant="h6">{user}</Typography>
+          <Divider />
           {clearstreamTicketByOwnerFirstName[user].map(clearstreamTicket => {
             return <div key={clearstreamTicket.ticketId}>{clearstreamTicket.ticketTitle}</div>
           })}
